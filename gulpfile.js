b6aed5b7adbeb239f88e3b8d1820e229cadacede
SashaGurin <top-on3@mai.ru>
2022-01-16 22:37:20 +0300

    + header_pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,105 +1,57 @@
-const gulp = require("gulp"),
-  browserSync = require("browser-sync"),
-  autoPrefixer = require("gulp-autoprefixer"),
-  sass = require("gulp-sass")(require("sass")),
-  autoprefixer = require("gulp-autoprefixer"),
-  cleanCSS = require("gulp-clean-css"),
-  pug = require("gulp-pug"),
-  plumber = require("gulp-plumber"),
-//   concat = require('gulp-concat'),
-//   uglify = require('gulp-uglify');
+const gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    autoPrefixer = require('gulp-autoprefixer'),
+    sass = require('gulp-sass')(require('sass')),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleanCSS = require('gulp-clean-css'),
+    pug = require('gulp-pug'),
+    plumber = require('gulp-plumber')
 
 
 function browsersync() {
-  browserSync.init({
-    server: {
-      baseDir: "build",
-    },
-  });
+        browserSync.init({
+            server: {
+                baseDir: 'build'
+            }
+        })
 }
 
 function html() {
-  return gulp
-    .src("src/pug/*.pug")
-    .pipe(plumber())
-    .pipe(
-      pug({
-        pretty: true,
-      })
-    )
-    .pipe(plumber.stop())
-    .pipe(gulp.dest("build"))
-    .on("end", browserSync.reload);
+    return gulp.src('src/pug/*.pug')
+        .pipe(plumber())
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(plumber.stop())
+        .pipe(gulp.dest('build'))
+        .on('end', browserSync.reload)
 }
 
 function css() {
-  return gulp
-    .src("src/assets/scss/app.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(
-      autoprefixer({
-        overrideBrowserslist: ["last 2 versions"],
-        grid: "autoplace",
-      })
-    )
-    .pipe(cleanCSS())
-    .pipe(gulp.dest("build/assets/css"))
-    .pipe(browserSync.stream());
-}
-
-function js() {
-   return gulp
-     .src("src/assets/js/*.js")
-     .pipe(gulp.dest("build/assets/js"))
-     .pipe(browserSync.stream());
- }
-
-// function script() {
-//   return gulp
-//     .src(["node_modules/slick-carousel/slick/slick.js",
-//     "node_modules/magnific-popup/dist/jquery.magnific-popup.js"])
-//     .pipe(concat("libs.min.js"))
-//     .pipe(uglify())
-//     .pipe(gulp.dest("build/assets/js"))
-// }
+    return gulp.src('src/assets/scss/app.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            grid: 'autoplace',
+        }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('build/assets/css'))
+        .pipe(browserSync.stream())
+    }
 
 function images() {
-  return gulp
-    .src("src/assets/imgs/**/*")
-    .pipe(gulp.dest("build/assets/imgs"))
-    .pipe(browserSync.stream());
+    return gulp.src('src/assets/imgs/**/*')
+        .pipe(gulp.dest('build/assets/imgs'))
+        .pipe(browserSync.stream())
 }
 
 function watcher() {
-  gulp.watch("src/assets/imgs/**/*", images);
-  gulp.watch("src/assets/scss/**/*", css);
-  gulp.watch("src/pug/**/*", html);
-  gulp.watch("src/assets/js/**/*", js);
+    gulp.watch('src/assets/imgs/**/*', images)
+    gulp.watch('src/assets/scss/**/*', css)
+    gulp.watch('src/pug/**/*', html)
 }
 
 gulp.task(
-  "default",
-  gulp.parallel(browsersync, watcher, css, html, js, images)
+    'default',
+    gulp.parallel(browsersync, watcher, css, html, images)
 );
-
-// пример taska
-// gulp.task('sass',function(){
-//    gulp.src('src/assets/scss/app.scss')
-//    .pipe(sass())
-//    .pipe(gulp.dest("build/assets/css"))
-// })
-
-// для минифицирования sass
-// .pipe(sass({outputStyle: 'compressed'}).on("error", sass.logError))
-// пример:
-// gulp.src('src/assets/scss/app.scss')
-//   .pipe(sass({outputStyle: 'compressed'}).on("error", sass.logError))
-//   .pipe(gulp.dest("build/assets/css"))
-// })
-
-// task rename переиминовывает конечные файлы
-// .pipe(rename({ suffix: ".min" }))
-// пример:
-// gulp.src("./src/main/text/hello.txt")
-//   .pipe(rename({ suffix: ".min" }))
-//   .pipe(gulp.dest("build/assets/css"));
